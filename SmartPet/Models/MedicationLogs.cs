ada0719bdf0b975746e74115ac506aa2f345c9dc
Sam Stewart <sstewart@luc.edu>
2026-03-28 17:05:38 -0500

    updated model fields

--- SmartPet/Models/MedicationLogs.cs
+++ SmartPet/Models/MedicationLogs.cs
@@ -8,14 +8,10 @@ namespace SmartPet.Models
 {
     public class MedicationLogs
     {
-		public int MedicationLogId { get; set; }  // Primary Key
-		public string MedicationName { get; set; } // Name of the medication
-		public string Dosage { get; set; } // Dosage of the medication (e.g., "10 mg")
-		public DateTime AdministeredAt { get; set; } // Date and time when the medication was given
-		public string Notes { get; set; } // Optional notes (e.g., Special Instructions, Side Effects)
-
-		// Foreign key to Pet
-		public int PetId { get; set; }
+		public int id { get; set; }  // Primary Key
+		public int medicationId { get; set; } // Name of the medication
+		public string amount { get; set; } // Dosage of the medication (e.g., "10 mg")
+		public DateTime administeredAt { get; set; } // Date and time when the medication was given
 
 	}
 }
