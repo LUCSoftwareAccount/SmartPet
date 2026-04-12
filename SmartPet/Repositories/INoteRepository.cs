3489a86a5ec43460eaf812a8e62cb40f5e668a13
Amina Sodnom <asodnomtseren@luc.edu>
2026-04-12 13:45:14 -0500

    wip - changes not ready to push

---
+++ SmartPet/Repositories/INoteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SmartPet.Models;
+
+namespace SmartPet.Repositories
+{
+	public interface INoteRepository
+	{
+		Task<IEnumerable<Note>> GetAllNotesAsync();
+		Task<IEnumerable<Note>> GetNotesByPetIdAsync(int petId);
+		Task<Note> GetNoteByIdAsync(int id);
+		Task AddNoteAsync(Note note);
+		Task UpdateNoteAsync(Note note);
+		Task DeleteNoteAsync(int id);
+	}
+}
